fa029aeb1c47db78eae4668ab9447eeaff9a0434
nick <nick@techdox.nz>
2023-06-28 15:47:26 +1200

    added logic to functions

--- main.py
+++ main.py
@@ -1,71 +1,79 @@
 import paramiko
 import os
 
-# SSH connection details
-remote_hostname = ''
-remote_port = 22  # SSH default port
-remote_username = ''
-private_key_path = ''
+def run_backup(container_name, backup_file_name, backup_path, local_destination):
+    # SSH connection details
+    remote_hostname = '192.168.68.109'
+    remote_port = 22  # SSH default port
+    remote_username = 'techdox'
+    private_key_path = '/Users/nick/.ssh/id_rsa'
 
-# Inputs for container name, backup file name, and backup path
-container_name = input("Enter the container name to backup: ")
-backup_file_name = input("Enter the backup file name: ")
-backup_path = input("Enter the backup path: ")
+    # Command to execute on the remote server to stop the Docker container
+    stop_command = f'docker stop {container_name}'
 
-# Command to execute on the remote server to stop the Docker container
-stop_command = f'docker stop {container_name}'
+    # Command to execute on the remote server to start the Docker container
+    start_command = f'docker start {container_name}'
 
-# Command to execute on the remote server to start the Docker container
-start_command = f'docker start {container_name}'
+    # Command to execute on the remote server to create the backup
+    backup_command = f'docker run --rm --volumes-from {container_name} -v $(pwd):/backup ubuntu bash -c "cd {backup_path} && tar cvf /backup/{backup_file_name}.tar ."'
 
-# Command to execute on the remote server to create the backup
-backup_command = f'docker run --rm --volumes-from {container_name} -v $(pwd):/backup ubuntu bash -c "cd {backup_path} && tar cvf /backup/{backup_file_name}.tar ."'
+    # Create an SSH client
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Local destination path to save the backup file
-local_destination = input("Enter the local destination path to save the backup file: ")
+    try:
+        # Load the private key
+        private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
 
-# Create an SSH client
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # Connect to the remote server using key-based authentication
+        ssh_client.connect(remote_hostname, remote_port, remote_username, pkey=private_key)
 
-try:
-    # Load the private key
-    private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+        # Execute the command to stop the Docker container
+        ssh_client.exec_command(stop_command)
+        print("Docker container stopped.")
 
-    # Connect to the remote server using key-based authentication
-    ssh_client.connect(remote_hostname, remote_port, remote_username, pkey=private_key)
+        # Execute the backup command on the remote server
+        stdin, stdout, stderr = ssh_client.exec_command(backup_command)
+        print(stdout.read().decode())
 
-    # Execute the command to stop the Docker container
-    ssh_client.exec_command(stop_command)
-    print("Docker container stopped.")
+        # Create an SCP client from the SSH client
+        scp_client = ssh_client.open_sftp()
 
-    # Execute the backup command on the remote server
-    stdin, stdout, stderr = ssh_client.exec_command(backup_command)
-    print(stdout.read().decode())
+        # Retrieve the home directory of the remote user
+        remote_home_directory = ssh_client.exec_command('echo $HOME')[1].read().decode().strip()
 
-    # Create an SCP client from the SSH client
-    scp_client = ssh_client.open_sftp()
+        # Construct the remote backup file path
+        remote_backup_file_path = os.path.join(remote_home_directory, f'{backup_file_name}.tar')
 
-    # Retrieve the home directory of the remote user
-    remote_home_directory = ssh_client.exec_command('echo $HOME')[1].read().decode().strip()
+        # Construct the local backup file path
+        local_backup_file_path = os.path.join(local_destination, f'{backup_file_name}.tar')
 
-    # Construct the remote backup file path
-    remote_backup_file_path = os.path.join(remote_home_directory, f'{backup_file_name}.tar')
+        # Download the backup file from the remote server to the local machine
+        scp_client.get(remote_backup_file_path, local_backup_file_path)
+        print("Backup file downloaded successfully.")
 
-    # Construct the local backup file path
-    local_backup_file_path = os.path.join(local_destination, f'{backup_file_name}.tar')
+        # Execute the command to start the Docker container
+        ssh_client.exec_command(start_command)
+        print("Docker container started.")
 
-    # Download the backup file from the remote server to the local server
-    scp_client.get(remote_backup_file_path, local_backup_file_path)
-    print("Backup file downloaded successfully.")
+        # Close the SCP client
+        scp_client.close()
 
-    # Execute the command to start the Docker container
-    ssh_client.exec_command(start_command)
-    print("Docker container started.")
+    finally:
+        # Close the SSH connection
+        ssh_client.close()
 
-    # Close the SCP client
-    scp_client.close()
+def get_user_input():
+    container_name = input("Enter the container name to backup: ")
+    backup_file_name = input("Enter the backup file name: ")
+    backup_path = input("Enter the backup path: ")
+    local_destination = input("Enter the local destination path to save the backup file: ")
 
-finally:
-    # Close the SSH connection
-    ssh_client.close()
+    return container_name, backup_file_name, backup_path, local_destination
+
+def main():
+    container_name, backup_file_name, backup_path, local_destination = get_user_input()
+    run_backup(container_name, backup_file_name, backup_path, local_destination)
+
+if __name__ == '__main__':
+    main()
