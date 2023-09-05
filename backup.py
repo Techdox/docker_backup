4313cf2bfb5b640c16aab7fe55f409789b92d8e0
techdox <nick@techdox.nz>
2023-09-06 09:16:26 +1200

    refactored code

--- backup.py
+++ backup.py
@@ -7,63 +7,53 @@ REMOTE_PORT = 22  # SSH default port
 REMOTE_USERNAME = ''
 PRIVATE_KEY_PATH = ''
 
-def run_backup(container_name, backup_file_name, backup_path, local_destination):
-    # Command to execute on the remote server to stop the Docker container
+def run_remote_command(ssh_client, command):
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+
+def backup_container(container_name, backup_file_name, backup_path, local_destination):
+    # Commands
     stop_command = f'docker stop {container_name}'
-
-    # Command to execute on the remote server to start the Docker container
     start_command = f'docker start {container_name}'
-
-    # Command to execute on the remote server to create the backup
-    backup_command = f'docker run --rm --volumes-from {container_name} -v $(pwd):/backup ubuntu bash -c "cd {backup_path} && tar cvf /backup/{backup_file_name}.tar ."'
+    backup_command = (
+        f'docker run --rm --volumes-from {container_name} '
+        f'-v $(pwd):/backup ubuntu bash -c '
+        f'"cd {backup_path} && tar cvf /backup/{backup_file_name}.tar ."'
+    )
 
     # Create an SSH client
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # Load the private key
-    private_key = paramiko.RSAKey.from_private_key_file(PRIVATE_KEY_PATH)
-
     try:
+        # Load the private key
+        private_key = paramiko.RSAKey.from_private_key_file(PRIVATE_KEY_PATH)
+
         # Connect to the remote server using key-based authentication
         ssh_client.connect(REMOTE_HOSTNAME, REMOTE_PORT, REMOTE_USERNAME, pkey=private_key)
 
-        # Execute the command to stop the Docker container
-        ssh_client.exec_command(stop_command)
+        # Execute commands
+        run_remote_command(ssh_client, stop_command)
         print("Docker container stopped.")
+        run_remote_command(ssh_client, backup_command)
+        run_remote_command(ssh_client, start_command)
+        print("Docker container started.")
 
-        # Execute the backup command on the remote server
-        stdin, stdout, stderr = ssh_client.exec_command(backup_command)
-        print(stdout.read().decode())
-        
-        # Print the standard error
-        print(stderr.read().decode())
+        # Construct remote and local file paths
+        remote_backup_file_path = os.path.join(ssh_client.exec_command('echo $HOME')[1].read().decode().strip(), f'{backup_file_name}.tar')
+        local_backup_file_path = os.path.join(local_destination, f'{backup_file_name}.tar')
 
         # Create an SCP client from the SSH client
         scp_client = ssh_client.open_sftp()
-
-        # Retrieve the home directory of the remote user
-        remote_home_directory = ssh_client.exec_command('echo $HOME')[1].read().decode().strip()
-
-        # Construct the remote backup file path
-        remote_backup_file_path = os.path.join(remote_home_directory, f'{backup_file_name}.tar')
-
-        # Construct the local backup file path
-        local_backup_file_path = os.path.join(local_destination, f'{backup_file_name}.tar')
 
         # Download the backup file from the remote server to the local machine
         scp_client.get(remote_backup_file_path, local_backup_file_path)
         print("Backup file downloaded successfully.")
 
-        # Execute the command to start the Docker container
-        ssh_client.exec_command(start_command)
-        print("Docker container started.")
-
     finally:
-        # Close the SCP client
+        # Close the SCP client and SSH connection
         scp_client.close()
-        
-        # Close the SSH connection
         ssh_client.close()
 
 def get_user_input():
@@ -76,7 +66,7 @@ def get_user_input():
 
 def main():
     container_name, backup_file_name, backup_path, local_destination = get_user_input()
-    run_backup(container_name, backup_file_name, backup_path, local_destination)
+    backup_container(container_name, backup_file_name, backup_path, local_destination)
 
 if __name__ == '__main__':
     main()
--- restore.py
+++ restore.py
@@ -1,5 +1,4 @@
 import paramiko
-import os
 
 # Constants for SSH connection details
 REMOTE_HOSTNAME = ''
@@ -7,57 +6,48 @@ REMOTE_PORT = 22  # SSH default port
 REMOTE_USERNAME = ''
 PRIVATE_KEY_PATH = ''
 
-def run_restore(container_name, backup_file_name, backup_path, backup_source):
-    # Command to execute on the remote server to stop the Docker container
-    stop_command = f'docker stop {container_name}'
+def run_remote_command(ssh_client, command):
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+    print(stdout.read().decode())
+    print(stderr.read().decode())
 
-    # Command to execute on the remote server to start the Docker container
-    start_command = f'docker start {container_name}'
-
-    # Command to execute on the remote server to restore the backup
-    restore_command = f'docker run --rm --volumes-from {container_name} -v {backup_source}:/backup ubuntu bash -c "rm -rf {backup_path}/* && cd {backup_path} && tar xvf /backup/{backup_file_name}.tar"'
+def main():
+    # Get user input
+    container_name = input("Enter the container name to restore: ")
+    backup_file_name = input("Enter the backup file name: ")
+    backup_path = input("Enter the backup path: ")
+    backup_source = input("Enter the backup source on the remote server: ")
 
     # Create an SSH client
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # Load the private key
-    private_key = paramiko.RSAKey.from_private_key_file(PRIVATE_KEY_PATH)
-
     try:
+        # Load the private key
+        private_key = paramiko.RSAKey.from_private_key_file(PRIVATE_KEY_PATH)
+
         # Connect to the remote server using key-based authentication
         ssh_client.connect(REMOTE_HOSTNAME, REMOTE_PORT, REMOTE_USERNAME, pkey=private_key)
 
-        # Execute the command to stop the Docker container
-        ssh_client.exec_command(stop_command)
+        # Commands
+        stop_command = f'docker stop {container_name}'
+        start_command = f'docker start {container_name}'
+        restore_command = (
+            f'docker run --rm --volumes-from {container_name} '
+            f'-v {backup_source}:/backup ubuntu bash -c '
+            f'"rm -rf {backup_path}/* && cd {backup_path} && tar xvf /backup/{backup_file_name}.tar"'
+        )
+
+        # Execute commands
+        run_remote_command(ssh_client, stop_command)
         print("Docker container stopped.")
-
-        # Execute the restore command on the remote server
-        stdin, stdout, stderr = ssh_client.exec_command(restore_command)
-        print(stdout.read().decode())
-
-        # Print the standard error
-        print(stderr.read().decode())
-
-        # Execute the command to start the Docker container
-        ssh_client.exec_command(start_command)
+        run_remote_command(ssh_client, restore_command)
+        run_remote_command(ssh_client, start_command)
         print("Docker container started.")
 
     finally:
         # Close the SSH connection
         ssh_client.close()
-
-def get_user_input():
-    container_name = input("Enter the container name to restore: ")
-    backup_file_name = input("Enter the backup file name: ")
-    backup_path = input("Enter the backup path: ")
-    backup_source = input("Enter the backup source on the remote server")
-
-    return container_name, backup_file_name, backup_path, backup_source
-
-def main():
-    container_name, backup_file_name, backup_path, backup_source = get_user_input()
-    run_restore(container_name, backup_file_name, backup_path, backup_source)
 
 if __name__ == '__main__':
     main()
